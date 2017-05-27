aee81ff9feb1f010143de6160d7491cc7ba6e3f9
Scofieldyu <931180361@qq.com>
2017-05-27 23:39:53 +0800

    first commit

---
+++ run.py
@@ -0,0 +1,28 @@
+# encoding: utf-8
+from flask import Flask, render_template, redirect,make_response
+from functools import wraps
+
+app = Flask(__name__)
+
+def allow_cross_domain(fun):
+    @wraps(fun)
+    def wrapper_fun(*args, **kwargs):
+        rst = make_response(fun(*args, **kwargs))
+        rst.headers['Access-Control-Allow-Origin'] = '*'
+        rst.headers['Access-Control-Allow-Methods'] = 'PUT,GET,POST,DELETE'
+        allow_headers = "Referer,Accept,Origin,User-Agent"
+        rst.headers['Access-Control-Allow-Headers'] = allow_headers
+        return rst
+    return wrapper_fun
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login',methods=['POST','GET'])
+@allow_cross_domain
+def login():
+	return  "success"
+
+if __name__ == '__main__':
+    app.run(debug=True)
