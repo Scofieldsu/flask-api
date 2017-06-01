d223958af24d61d940cb6c4ab1a1ffb78e78cac3
Scofieldyu <931180361@qq.com>
2017-06-02 00:37:18 +0800

    modify run.py space

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 # encoding: utf-8
-from flask import Flask, render_template, redirect,make_response,jsonify
+from flask import Flask, render_template, redirect, make_response, jsonify
 from functools import wraps
 
 app = Flask(__name__)
+
 
 def allow_cross_domain(fun):
     @wraps(fun)
@@ -13,30 +14,36 @@ def allow_cross_domain(fun):
         allow_headers = "Referer,Accept,Origin,User-Agent"
         rst.headers['Access-Control-Allow-Headers'] = allow_headers
         return rst
+
     return wrapper_fun
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/login',methods=['POST','GET'])
+
+@app.route('/login', methods=['POST', 'GET'])
 @allow_cross_domain
 def login():
-    return  "success"
+    return "success"
 
-@app.route('/logout',methods=['POST','GET'])
+
+@app.route('/logout', methods=['POST', 'GET'])
 @allow_cross_domain
 def logout():
-    return  "success"
+    return "success"
 
-@app.route('/get_all_api',methods=['POST','GET'])
+
+@app.route('/get_all_api', methods=['POST', 'GET'])
 @allow_cross_domain
 def get_all_api():
-    result ={'login':{'name': 'login', 'description': '登录接口', 'params': {'loginname': 'str'}},
-             'get_all_api':{'name':'get_all_api','description':'获取所有接口信息','params':{}},
-             'logout':{'name':'logout','description': '退出','params': {}},
-            'allKey':['login','get_all_api','logout']} 
-    return  jsonify(result)
+    result = {'login': {'name': 'login', 'description': '登录接口', 'params': {'loginname': 'str'}},
+              'get_all_api': {'name': 'get_all_api', 'description': '获取所有接口信息', 'params': {}},
+              'logout': {'name': 'logout', 'description': '退出', 'params': {}},
+              'allKey': ['login', 'get_all_api', 'logout']}
+    return jsonify(result)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
